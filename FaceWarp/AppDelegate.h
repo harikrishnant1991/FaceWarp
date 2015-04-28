7f72ebf895c564ba55bdc7eff36182c6664f11fc
Sreekanth <sreekanth@Apples-MacBook-Pro-2.local>
2015-04-28 10:50:33 +0530

    Initial Commit

---
+++ FaceWarp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FaceWarp
+//
+//  Created by Sreekanth on 28/04/15.
+//  Copyright (c) 2015 FyrWeel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FaceWarp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FaceWarp
+//
+//  Created by Sreekanth on 28/04/15.
+//  Copyright (c) 2015 FyrWeel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
